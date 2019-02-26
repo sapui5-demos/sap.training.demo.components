5580c1e1d89f8371c5630751e3def0758826a0d8
sapui5-demos <chriskne@gmail.com>
2019-02-26 08:29:12 +0000

    App Descriptor API

--- webapp/products/Component.js
+++ webapp/products/Component.js
@@ -1,14 +1,24 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent"
-], function(UIComponent) {
+], function (UIComponent) {
 	"use strict";
 
 	return UIComponent.extend("sap.training.products.Component", {
 
 		metadata: {
 			manifest: "json"
-		}
+		},
 
+		init: function () {
+			
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			this.mManifest = this.getManifest();
+			this.mAppData = this.getManifestEntry("sap.app");
+			this.mConfig = this.getManifestEntry("/sap.ui5/config");
+
+		}
 	});
 
 });
--- webapp/products/controller/Products.controller.js
+++ webapp/products/controller/Products.controller.js
@@ -1,9 +1,19 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+], function (Controller) {
 	"use strict";
 
 	return Controller.extend("sap.training.products.controller.Products", {
+
+		onPress: function (oEvent) {
+
+			var oComponent = this.getOwnerComponent();
+
+			var mManifest = oComponent.getManifest();
+			var mAppData = oComponent.getManifestEntry("sap.app");
+			var mConfig = oComponent.getManifestEntry("/sap.ui5/config");
+
+		}
 
 	});
 
